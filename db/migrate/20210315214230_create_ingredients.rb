abd4b1bc0951d5bdb44a88f7342fb4ea4db15062
Evan <evanfujita@gmail.com>
2021-08-31 19:27:00 -0500

    added database validaitons

--- db/migrate/20210315214230_create_ingredients.rb
+++ db/migrate/20210315214230_create_ingredients.rb
@@ -1,12 +1,12 @@
 class CreateIngredients < ActiveRecord::Migration[6.1]
   def change
     create_table :ingredients do |t|
-      t.string :name
-      t.integer :quantity
-      t.string :quantity_unit
-      t.integer :par
-      t.string :category_id
-      t.string :vendor_id
+      t.string :name, null: false
+      t.integer :quantity, default: 1
+      t.string :quantity_unit, null: false
+      t.integer :par, null: false
+      t.string :category_id, null: false
+      t.string :vendor_id, null: false
 
       t.timestamps
     end
--- db/migrate/20210325232035_create_vendors.rb
+++ db/migrate/20210325232035_create_vendors.rb
@@ -1,8 +1,8 @@
 class CreateVendors < ActiveRecord::Migration[6.1]
   def change
     create_table :vendors do |t|
-      t.string :name
-      t.string :representative
+      t.string :name, null: false
+      t.string :representative, null: false
       t.string :phone
       t.string :email
       
